f061ab9ae0dedfc7715112c0579ca7cf4135a8ff
Lucas Passos <lucas.spassos@outlook.com>
2019-04-30 22:30:23 -0300

    Criação do Cenário

--- Shooter/src/mygame/Main.java
+++ Shooter/src/mygame/Main.java
@@ -3,8 +3,11 @@ package mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Transform;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -18,17 +21,59 @@ public class Main extends SimpleApplication {
         Main app = new Main();
         app.start();
     }
+    
+    private Node cenario;
+
+    public Main() {
+    }
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+       
+        cam.setLocation(new Vector3f(0,0.8f,10));
+        //flyCam.setEnabled(false);//trava movimentação da camera com o mouse
+        
+        cenario = new Node("Cenario");
+        Box chao = new Box(1, 0.05f, 10);
+        Geometry geomChao = new Geometry("Chao", chao);
+        Material matChao = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        matChao.setColor("Color", ColorRGBA.Blue);
+        geomChao.setMaterial(matChao);
+        cenario.attachChild(geomChao);
+        
+        Box paredeEsq = new Box(0.05f, 0.6f, 10);
+        Geometry geomParedeEsq = new Geometry("ParedeEsq", paredeEsq);
+        Material matParede = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        matParede.setColor("Color", ColorRGBA.Red);
+        geomParedeEsq.setMaterial(matParede);
+        geomParedeEsq.setLocalTranslation(-1, 0.6f,0);
+        cenario.attachChild(geomParedeEsq);
+        
+        Box paredeDir = new Box(0.05f, 0.6f, 10);
+        Geometry geomParedeDir = new Geometry("ParedeEsq", paredeDir);
+        geomParedeDir.setMaterial(matParede);
+        geomParedeDir.setLocalTranslation(1, 0.6f,0);
+        cenario.attachChild(geomParedeDir);
+        
+        Box teto = new Box(1, 0.05f, 10);
+        Geometry geomTeto = new Geometry("Teto", teto);
+        Material matTeto = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        matTeto.setColor("Color", ColorRGBA.Orange);
+        geomTeto.setMaterial(matTeto);
+        geomTeto.setLocalTranslation(0, 1.2f,0);
+        cenario.attachChild(geomTeto);
+        
+        
+        Box paredeFinal = new Box(1, 0.6f, 0.1f);
+        Geometry geomParedeFinal = new Geometry("ParedeFinal", paredeFinal);
+        Material matParedeFinal = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        matParedeFinal.setColor("Color", ColorRGBA.Magenta);
+        geomParedeFinal.setMaterial(matParedeFinal);
+        geomParedeFinal.setLocalTranslation(0,0.6f,-10);
+        cenario.attachChild(geomParedeFinal);
+        
+        
+        rootNode.attachChild(cenario);
     }
 
     @Override
